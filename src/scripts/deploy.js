9f52ac1c32886f8ce8e69f49f62dc8a8e747790f
Alexey Zaytsev <alexej3ajtsev@icloud.com>
2023-02-07 08:53:31 +0300

    build: changed deploy to rsync

--- src/scripts/deploy.js
+++ src/scripts/deploy.js
@@ -2,9 +2,8 @@ require('dotenv').config();
 const { execSync } = require('child_process');
 
 const deploy = () => {
-    const command = `scp -C -o "BatchMode yes" -r public/* ${process.env.DEPLOY_USER}@${process.env.DEPLOY_HOST}:${process.env.DEPLOY_PATH}`;
+    const command = `rsync --no-o --no-g --omit-dir-times -a -P  -e ssh public/ ${process.env.DEPLOY_USER}@${process.env.DEPLOY_HOST}:${process.env.DEPLOY_PATH}`;
     const result = execSync(command, { stdio: 'inherit', encoding: 'utf8' });
-    console.log(result);
 }
 
 deploy();
