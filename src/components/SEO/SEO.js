3e436830a521b692ad527e069f4e351c98c5f419
Alexey Zaytsev <alexej3ajtsev@icloud.com>
2023-05-21 13:36:20 +0700

    feat: added favicon

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -24,7 +24,7 @@ export const SEO = ({ title, description, pathname, children, image }) => {
             <meta name="twitter:description" content={seo.description} />
             <meta name="twitter:image" content={seo.image} />
             <meta name="twitter:creator" content={seo.twitterUsername} />
-            <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>👤</text></svg>" />
+            {process.env.FAVICON_DOMAIN && <link rel="icon" href={process.env.FAVICON_DOMAIN + "/favicon.ico"} type="image/x-icon"></link>}
             {children}
         </>
     )
